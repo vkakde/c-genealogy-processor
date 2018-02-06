4828ecf54e418b4a374a6f3937a67ccf2b95ebe2
Louis Rozencwajg-Hays <louisrh999@gmail.com>
2018-02-06 10:01:36 -0500

    Adde family and individual class and created list to store them

--- main.cpp
+++ main.cpp
@@ -10,115 +10,175 @@
 #include <fstream>
 #include <algorithm>
 
-class gedcom{
+
+
+class Individual {
+public:
+    //No getters/setters for this many fields - it shouldn't be necessary
+    std::string id;
+    std::string name;
+    std::string sex;
+    std::string birthDay;
+    std::string deathDate;
+    std::string famcId;
+    std::string famsId;
+
+    Individual(std::string cId) {
+        id = cId;
+    }
+
+};
+
+class Family {
+public:
+    std::string id;
+    std::string husb;
+    std::string wife;
+    std::string marrDate;
+    std::string divorceDate;
+    std::vector<Individual> children;
+
+    Family(std::string cId) {
+        id = cId;
+    }
+    
+};
+
+
+class gedcom {
     std::map<std::string, int> supportedTagsAndLevels;
     std::string gedcomData;
+    std::vector<Family> familyList = {};
+    std::vector<Individual> individualList = {};
+    std::string currentId;
+    bool isInd;
+    bool deathDate = false;
+    bool birthDate = false;
+    bool marrDate = false;
+    bool divDate = false;
 
     // constructors
-    public:
-    gedcom(){
-        supportedTagsAndLevels = {{"INDI",0}, {"NAME",1}, {"SEX",1}, {"BIRT",1}, {"DEAT",1}, {"FAMC",1}, {"FAMS",1}, {"FAM",0}, {"MARR",1}, {"HUSB",1}, {"WIFE",1},
-                        {"CHIL",1}, {"DIV",1}, {"DATE",2}, {"HEAD",0}, {"TRLR",0}, {"NOTE",0}};
+public:
+    gedcom() {
+        supportedTagsAndLevels = { { "INDI",0 },{ "NAME",1 },{ "SEX",1 },{ "BIRT",1 },{ "DEAT",1 },{ "FAMC",1 },{ "FAMS",1 },{ "FAM",0 },{ "MARR",1 },{ "HUSB",1 },{ "WIFE",1 },
+        { "CHIL",1 },{ "DIV",1 },{ "DATE",2 },{ "HEAD",0 },{ "TRLR",0 },{ "NOTE",0 } };
     }
 
     // public functions
-    void readGedFile(){
+    void readGedFile() {
         // prompt user input
-        std::cout<<"\nEnter file path+name of .ged file relative to workspace (ex: gedcom/file.ged): ";
+        std::cout << "\nEnter file path+name of .ged file relative to workspace (ex: gedcom/file.ged): ";
         std::string file;
-        std::cin>>file;
+        std::cin >> file;
 
         ///cite https://stackoverflow.com/a/19922123
         std::ifstream inFile;
         inFile.open(file);
-        if(!inFile){              // open file
-            std::cout<<"\nFile open failed. Restart program and try again.\n";
-            std::cout<<"Possible reasons for failure: \n\t1. Does the file exist?\n";
-            std::cout<<"\t2. Did you forget to add the .ged extension?\n";
-            std::cout<<"\t3. Mind the forward and reverse slashes depending on your OS.";
-            std::cout<<"\tFor simplicity - place input ged in same folder as the source file.\n";
+        if (!inFile) {              // open file
+            std::cout << "\nFile open failed. Restart program and try again.\n";
+            std::cout << "Possible reasons for failure: \n\t1. Does the file exist?\n";
+            std::cout << "\t2. Did you forget to add the .ged extension?\n";
+            std::cout << "\t3. Mind the forward and reverse slashes depending on your OS.";
+            std::cout << "\tFor simplicity - place input ged in same folder as the source file.\n";
         }
         std::stringstream strStream;
         strStream << inFile.rdbuf();        // read file
         gedcomData = strStream.str();       // read file contents into string
     }
 
-    void printGedcomData(){
+    void printGedcomData() {
         std::cout << gedcomData << "\n";
     }
 
-    void printSupportedTags(){
-        for(auto& i : supportedTagsAndLevels){
-            std::cout<<"\n"<<i.first<<"  "<<i.second;
+    void printSupportedTags() {
+        for (auto& i : supportedTagsAndLevels) {
+            std::cout << "\n" << i.first << "  " << i.second;
         }
     }
 
-    bool isIndiOrFam(std::vector<std::string> lineElements){
+    /*
+    bool isIndiOrFam(std::vector<std::string> lineElements) {
+    ///cite https://stackoverflow.com/a/19266247/2535979
+    std::locale loc;
+    std::string lineElement_index2;
+    if (lineElements.size()>2) {
+    for (auto i : lineElements[2]) {
+    lineElement_index2 += std::tolower(i, loc);
+    }
+
+    if (lineElement_index2 == "indi" || lineElement_index2 == "fam") {
+    return true;
+    }
+    }
+    return false;
+    }
+    */
+    std::string isIndiOrFam(std::vector<std::string> lineElements) {
         ///cite https://stackoverflow.com/a/19266247/2535979
         std::locale loc;
         std::string lineElement_index2;
-        if(lineElements.size()>2){
-            for(auto i : lineElements[2]){
-                lineElement_index2 += std::tolower(i,loc);
+        if (lineElements.size()>2) {
+            for (auto i : lineElements[2]) {
+                lineElement_index2 += std::tolower(i, loc);
             }
-        
-            if(lineElement_index2 == "indi" || lineElement_index2 == "fam"){
-                return true;
+
+            if (lineElement_index2 == "indi" || lineElement_index2 == "fam") {
+                return lineElement_index2;
+            }
+        }
+        return "";
+    }
+
+    bool isTagValid(std::string tagLevel, std::string tagName) {
+        // convert tag under test to lower case
+        ///cite https://stackoverflow.com/a/19266247/2535979
+        std::locale loc;
+        std::string testTagName_LowerCase;
+        for (auto i : tagName) {
+            testTagName_LowerCase += std::tolower(i, loc);
+        }
+
+        // convert all supported tags to lower case (for comparison) 
+        std::map<std::string, std::string> supportedTagsAndLevels_LowerCase;
+        for (auto tag : supportedTagsAndLevels) {
+            std::string loweredTag = "";
+            std::string level = std::to_string(tag.second);
+            for (auto i : tag.first) {
+                loweredTag += std::tolower(i, loc);
+            }
+            supportedTagsAndLevels_LowerCase.insert(std::make_pair(loweredTag, level));
+        }
+
+        // if tag under test matches any supported tag and the tag's level is correct, it is valid. Return true.
+        for (auto i : supportedTagsAndLevels_LowerCase) {
+            if (i.first == testTagName_LowerCase) {
+                // Okay, tag is supported. But is the level valid?
+                if (i.second == tagLevel) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    bool isTagValid(std::string tagLevel, std::string tagName){
-        // convert tag under test to lower case
-        ///cite https://stackoverflow.com/a/19266247/2535979
-        std::locale loc;
-        std::string testTagName_LowerCase;
-        for(auto i : tagName){
-            testTagName_LowerCase += std::tolower(i,loc);
-        }       
-
-        // convert all supported tags to lower case (for comparison) 
-        std::map<std::string, std::string> supportedTagsAndLevels_LowerCase;
-        for(auto tag: supportedTagsAndLevels){
-            std::string loweredTag = "";
-            std::string level = std::to_string(tag.second);
-            for(auto i : tag.first){
-                loweredTag += std::tolower(i,loc);
-            }
-            supportedTagsAndLevels_LowerCase.insert(std::make_pair(loweredTag, level));       
-        }
-
-        // if tag under test matches any supported tag and the tag's level is correct, it is valid. Return true.
-        for(auto i: supportedTagsAndLevels_LowerCase){
-            if(i.first == testTagName_LowerCase){
-                // Okay, tag is supported. But is the level valid?
-                if(i.second == tagLevel){
-                    return true;
-                }
-            }
-        }
-        return false;        
-    }
-
     // parse and process a single line of gedcom data
-    void parseLine(std::string unparsedGedcomLine){
+    void parseLine(std::string unparsedGedcomLine) {
         std::vector<std::string> lineElements;
         std::string parsedGedcomLine = "";
-        
+
         /** break line into individual elements (id/tags/arguments) **/
-        for(auto i=0; i<unparsedGedcomLine.length(); i++){
+        for (auto i = 0; i<unparsedGedcomLine.length(); i++) {
             std::string lineElement = "";
             ///remark gedcom lines can have up to 3 attributes. 
             ///       Thus, every literal past second attribute automatically belongs to 3rd attribute.
-            if(lineElements.size()==2){
-                while(i<unparsedGedcomLine.length()){
-                    lineElement+=unparsedGedcomLine[i++];
+            if (lineElements.size() == 2) {
+                while (i<unparsedGedcomLine.length()) {
+                    lineElement += unparsedGedcomLine[i++];
                 }
             }
-            else{
-                while(unparsedGedcomLine[i]!=' ' && i<unparsedGedcomLine.length()){
-                    lineElement+=unparsedGedcomLine[i++];
+            else {
+                while (unparsedGedcomLine[i] != ' ' && i<unparsedGedcomLine.length()) {
+                    lineElement += unparsedGedcomLine[i++];
                 }
             }
 
@@ -132,7 +192,9 @@ class gedcom{
         parsedGedcomLine += '|';
 
         // process second and third elements depending on tag (is it INDI/FAM)
-        if(!isIndiOrFam(lineElements)){
+        std::string indiOrFam = isIndiOrFam(lineElements);
+        if (indiOrFam == "") {
+            //TODO: Add attribute to individual or Fam (pass in lineElements[1],lineElements[2]) 
             parsedGedcomLine += lineElements[1];
 
             // add delimiter
@@ -140,22 +202,74 @@ class gedcom{
 
             // is tag valid?
             ///remark A valid tag is one that is within the scope of cs555 project, refer doc "Project Overview"
-            if(isTagValid(lineElements[0], lineElements[1])){
+            if (isTagValid(lineElements[0], lineElements[1])) {
                 parsedGedcomLine += "Y";
             }
-            else{
+            else {
                 parsedGedcomLine += "N";
             }
 
-            if(lineElements.size()>2){
+            if (lineElements.size()>2) {
                 // add delimiter
                 parsedGedcomLine += '|';
 
                 // append arguments
                 parsedGedcomLine += lineElements[2];
+
+                //We know that there's a tag and a corresponding attribute, so add it:
+                if (lineElements[1] == "DATE") {
+                    if (deathDate) {
+                        addAttribute("DEAT", lineElements[2]);
+                        deathDate = false;
+                    }
+                    if (birthDate) {
+                        addAttribute("BIRT", lineElements[2]);
+                        birthDate = false;
+                    }
+                    if (marrDate) {
+                        addAttribute("MARR", lineElements[2]);
+                        marrDate = false;
+                    }
+                    if (divDate) {
+                        addAttribute("DIV", lineElements[2]);
+                        divDate = false;
+                    }
+                }
+                else{
+                    addAttribute(lineElements[1], lineElements[2]);
+                }
+            }
+            //These should only consist of the tags before dates. Save this tag so we can get its date on the next iteration 
+            else {
+                if (lineElements[1] == "DEAT") {
+                    deathDate = true;
+                }
+                if (lineElements[1] == "BIRT") {
+                    birthDate = true;
+                }
+                if (lineElements[1] == "MARR") {
+                    marrDate = true;
+                }
+                if (lineElements[1] == "DIV") {
+                    divDate = true;
+                }
             }
         }
-        else{
+        else { 
+            if (indiOrFam == "fam") {
+                Family family(lineElements[1]);
+                familyList.push_back(family);
+
+                isInd = false;
+            }
+            else {
+                Individual individual(lineElements[1]);
+                individualList.push_back(individual);
+
+                isInd = true;
+            }
+
+
             parsedGedcomLine += lineElements[2];
 
             // add delimiter
@@ -163,14 +277,14 @@ class gedcom{
 
             // is tag valid?
             ///remark A valid tag is one that is within the scope of cs555 project, refer doc "Project Overview"
-            if(isTagValid(lineElements[0], lineElements[2])){
+            if (isTagValid(lineElements[0], lineElements[2])) {
                 parsedGedcomLine += "Y";
             }
-            else{
+            else {
                 parsedGedcomLine += "N";
             }
 
-            if(lineElements.size()>2){
+            if (lineElements.size()>2) {
                 // add delimiter
                 parsedGedcomLine += '|';
 
@@ -180,25 +294,89 @@ class gedcom{
         }
 
         // print unparsed line
-        std::cout<<"\n--> "<<unparsedGedcomLine;
+        std::cout << "\n--> " << unparsedGedcomLine;
 
         // print parsed line
-        std::cout<<"\n<-- "<<parsedGedcomLine;
+        std::cout << "\n<-- " << parsedGedcomLine;
     }
 
     // parse and iteratively process all lines of gedcom data
-    void parseAll(){
-        for(auto i=0; i<gedcomData.length(); i++){
-            std::string gedcomLine="";
-            while(gedcomData[i]!='\n' && gedcomData[i]!='\0' && gedcomData[i]!='\r' && i<gedcomData.length()){
-                gedcomLine+=gedcomData[i++];
+    void parseAll() {
+        for (auto i = 0; i<gedcomData.length(); i++) {
+            std::string gedcomLine = "";
+            while (gedcomData[i] != '\n' && gedcomData[i] != '\0' && gedcomData[i] != '\r' && i<gedcomData.length()) {
+                gedcomLine += gedcomData[i++];
             }
             parseLine(gedcomLine);
         }
+
+        /* Test to see if lists were created correctly:
+        for(int i = 0; i < individualList.size(); i++){
+            std::cout << individualList[i].id << std::endl;
+            std::cout << individualList[i].name << std::endl;
+            
+        }
+        for(int i = 0; i < familyList.size(); i++){
+            std::cout << familyList[i].id << std::endl;
+        }
+        */
     }
+
+    void addAttribute(std::string tag, std::string attribute) {
+        int index;
+        if (isInd) {
+            index = individualList.size()-1;
+            if(index <= 0){
+                return;
+            }
+            //TODO: Add the attribute to the last individual in the vector (individualList)
+            if (tag == "NAME") {
+                individualList[index].name = attribute;
+            }
+            if (tag == "SEX") {
+                individualList[index].sex = attribute;
+            }
+            if (tag == "FAMS") {
+                individualList[index].famsId = attribute;
+            }
+            if (tag == "FAMC") {
+                individualList[index].famcId = attribute;
+            }
+            if (tag == "BIRT") {
+                individualList[index].birthDay = attribute;
+            }
+            if (tag == "DEAT") {
+                individualList[index].deathDate = attribute;
+            }
+        }
+        else {
+            index = familyList.size() - 1;
+            if(index <= 0){
+                return;
+            }
+            //TODO: Add the attribute to the last family in the vector (individualList)
+            if (tag == "HUSB") {
+                familyList[index].husb = attribute;
+            }
+            if (tag == "WIFE") {
+                familyList[index].wife = attribute;
+            }
+            if (tag == "CHIL") {
+                familyList[index].children.push_back(attribute);
+            }
+            if (tag == "MARR") {
+                familyList[index].marrDate = attribute;
+            }
+            if (tag == "DIV") {
+                familyList[index].divorceDate = attribute;
+            }
+        }
+    }
+
+
 };
 
-int main(){
+int main() {
     // create gedcom object instance
     gedcom gedcomInstance;
 
@@ -209,7 +387,7 @@ int main(){
     // gedcomInstance.printGedcomData();
 
     // parse gedcom data
-    gedcomInstance.parseAll();    
+    gedcomInstance.parseAll();
 
     return 0;
 }
